c4de0fdf81f4ebd25065e358f2aa5d088e8524a0
Gauri1804 <gauravb272@gmail.com>
2026-06-28 15:16:22 +0530

    bug fix

--- DB/Database.js
+++ DB/Database.js
@@ -1,10 +1,42 @@
+// import mongoose from "mongoose";
+
+// export const connectDB = async (req, res) => {
+//     const db = process.env.MONGO_URL;
+
+//     const { connection } = await mongoose.connect(db, { useNewUrlParser: true });
+
+//     console.log(`MongoDB Connected to ${connection.host}`);
+
+// } 
+
+
 import mongoose from "mongoose";
 
-export const connectDB = async (req, res) => {
+// Maintain a global cache so serverless containers reuse the connection
+let isConnected = false;
+
+export const connectDB = async () => {
+    // Prevent Mongoose from waiting 10 seconds to show underlying errors
+    mongoose.set('bufferCommands', false);
+
+    if (isConnected) {
+        console.log("=> Using existing MongoDB connection");
+        return;
+    }
+
     const db = process.env.MONGO_URL;
+    if (!db) {
+        throw new Error("MONGO_URL environment variable is missing!");
+    }
 
-    const { connection } = await mongoose.connect(db, { useNewUrlParser: true });
+    try {
+        console.log("=> Connecting to MongoDB...");
+        const res = await mongoose.connect(db);
 
-    console.log(`MongoDB Connected to ${connection.host}`);
-
-} 
+        isConnected = res.connections[0].readyState === 1;
+        console.log(`MongoDB Connected to ${res.connection.host}`);
+    } catch (error) {
+        console.error("MongoDB connection error details:", error);
+        throw error;
+    }
+};
