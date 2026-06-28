7590a652ed06e585389a25acc004ecdbca92f9b6
Gauri1804 <gauravb272@gmail.com>
2026-06-28 14:45:30 +0530

    bug fix

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,22 +1,165 @@
+// import User from "../models/UserSchema.js";
+// import bcrypt from "bcrypt";
+
+// export const registerControllers = async (req, res, next) => {
+//     try{
+//         const {name, email, password} = req.body;
+
+//         // console.log(name, email, password);
+
+//         if(!name || !email || !password){
+//             return res.status(400).json({
+//                 success: false,
+//                 message: "Please enter All Fields",
+//             }) 
+//         }
+
+//         let user = await User.findOne({email});
+
+//         if(user){
+//             return res.status(409).json({
+//                 success: false,
+//                 message: "User already Exists",
+//             });
+//         }
+
+//         const salt = await bcrypt.genSalt(10);
+
+//         const hashedPassword = await bcrypt.hash(password, salt);
+
+//         // console.log(hashedPassword);
+
+//         let newUser = await User.create({
+//             name, 
+//             email, 
+//             password: hashedPassword, 
+//         });
+
+//         return res.status(200).json({
+//             success: true,
+//             message: "User Created Successfully",
+//             user: newUser
+//         });
+//     }
+//     catch(err){
+//         return res.status(500).json({
+//             success: false,
+//             message: err.message,
+//         });
+//     }
+
+// }
+// export const loginControllers = async (req, res, next) => {
+//     try{
+//         const { email, password } = req.body;
+
+//         // console.log(email, password);
+
+//         if (!email || !password){
+//             return res.status(400).json({
+//                 success: false,
+//                 message: "Please enter All Fields",
+//             }); 
+//         }
+
+//         const user = await User.findOne({ email });
+
+//         if (!user){
+//             return res.status(401).json({
+//                 success: false,
+//                 message: "User not found",
+//             }); 
+//         }
+
+//         const isMatch = await bcrypt.compare(password, user.password);
+
+//         if (!isMatch){
+//             return res.status(401).json({
+//                 success: false,
+//                 message: "Incorrect Email or Password",
+//             }); 
+//         }
+
+//         delete user.password;
+
+//         return res.status(200).json({
+//             success: true,
+//             message: `Welcome back, ${user.name}`,
+//             user,
+//         });
+
+//     }
+//     catch(err){
+//         return res.status(500).json({
+//             success: false,
+//             message: err.message,
+//         });
+//     }
+// }
+
+// export const setAvatarController = async (req, res, next)=> {
+//     try{
+
+//         const userId = req.params.id;
+
+//         const imageData = req.body.image;
+
+//         const userData = await User.findByIdAndUpdate(userId, {
+//             isAvatarImageSet: true,
+//             avatarImage: imageData,
+//         },
+//         { new: true });
+
+//         return res.status(200).json({
+//             isSet: userData.isAvatarImageSet,
+//             image: userData.avatarImage,
+//           });
+
+
+//     }catch(err){
+//         next(err);
+//     }
+// }
+
+// export const allUsers = async (req, res, next) => {
+//     try{
+//         const user = await User.find({_id: {$ne: req.params.id}}).select([
+//             "email",
+//             "username",
+//             "avatarImage",
+//             "_id",
+//         ]);
+
+//         return res.json(user);
+//     }
+//     catch(err){
+//         next(err);
+//     }
+// }
+
+
+
 import User from "../models/UserSchema.js";
 import bcrypt from "bcrypt";
 
 export const registerControllers = async (req, res, next) => {
-    try{
-        const {name, email, password} = req.body;
+    try {
+        console.log("========== REGISTER REQUEST ==========");
+        console.log("Body:", req.body);
+        console.log("Mongo URL Exists:", !!process.env.MONGO_URL);
 
-        // console.log(name, email, password);
+        const { name, email, password } = req.body;
 
-        if(!name || !email || !password){
+        if (!name || !email || !password) {
             return res.status(400).json({
                 success: false,
                 message: "Please enter All Fields",
-            }) 
+            });
         }
 
-        let user = await User.findOne({email});
+        let user = await User.findOne({ email });
 
-        if(user){
+        if (user) {
             return res.status(409).json({
                 success: false,
                 message: "User already Exists",
@@ -24,115 +167,161 @@ export const registerControllers = async (req, res, next) => {
         }
 
         const salt = await bcrypt.genSalt(10);
-
         const hashedPassword = await bcrypt.hash(password, salt);
 
-        // console.log(hashedPassword);
-
-        let newUser = await User.create({
-            name, 
-            email, 
-            password: hashedPassword, 
+        const newUser = await User.create({
+            name,
+            email,
+            password: hashedPassword,
         });
+
+        console.log("User Created Successfully:", newUser._id);
 
         return res.status(200).json({
             success: true,
             message: "User Created Successfully",
-            user: newUser
+            user: newUser,
         });
-    }
-    catch(err){
+    } catch (err) {
+        console.error("========== REGISTER ERROR ==========");
+        console.error("Name:", err.name);
+        console.error("Message:", err.message);
+        console.error("Code:", err.code);
+        console.error("Stack:", err.stack);
+
+        if (err.errors) {
+            console.error("Validation Errors:", err.errors);
+        }
+
+        console.error("Request Body:", req.body);
+        console.error("=====================================");
+
         return res.status(500).json({
             success: false,
-            message: err.message,
+            error: {
+                name: err.name,
+                message: err.message,
+                code: err.code || null,
+                stack:
+                    process.env.NODE_ENV === "development"
+                        ? err.stack
+                        : undefined,
+            },
         });
     }
+};
 
-}
 export const loginControllers = async (req, res, next) => {
-    try{
+    try {
+        console.log("========== LOGIN REQUEST ==========");
+        console.log(req.body);
+
         const { email, password } = req.body;
 
-        // console.log(email, password);
-  
-        if (!email || !password){
+        if (!email || !password) {
             return res.status(400).json({
                 success: false,
                 message: "Please enter All Fields",
-            }); 
+            });
         }
-    
+
         const user = await User.findOne({ email });
-    
-        if (!user){
+
+        if (!user) {
             return res.status(401).json({
                 success: false,
                 message: "User not found",
-            }); 
+            });
         }
-    
+
         const isMatch = await bcrypt.compare(password, user.password);
-    
-        if (!isMatch){
+
+        if (!isMatch) {
             return res.status(401).json({
                 success: false,
                 message: "Incorrect Email or Password",
-            }); 
+            });
         }
 
-        delete user.password;
+        const userObj = user.toObject();
+        delete userObj.password;
+
+        console.log("Login Success:", userObj._id);
 
         return res.status(200).json({
             success: true,
-            message: `Welcome back, ${user.name}`,
-            user,
+            message: `Welcome back, ${userObj.name}`,
+            user: userObj,
         });
+    } catch (err) {
+        console.error("========== LOGIN ERROR ==========");
+        console.error(err);
 
-    }
-    catch(err){
         return res.status(500).json({
             success: false,
-            message: err.message,
+            error: {
+                name: err.name,
+                message: err.message,
+                code: err.code || null,
+                stack:
+                    process.env.NODE_ENV === "development"
+                        ? err.stack
+                        : undefined,
+            },
         });
     }
-}
+};
 
-export const setAvatarController = async (req, res, next)=> {
-    try{
+export const setAvatarController = async (req, res, next) => {
+    try {
+        console.log("========== SET AVATAR ==========");
 
         const userId = req.params.id;
-       
         const imageData = req.body.image;
-      
-        const userData = await User.findByIdAndUpdate(userId, {
-            isAvatarImageSet: true,
-            avatarImage: imageData,
-        },
-        { new: true });
+
+        const userData = await User.findByIdAndUpdate(
+            userId,
+            {
+                isAvatarImageSet: true,
+                avatarImage: imageData,
+            },
+            { new: true }
+        );
 
         return res.status(200).json({
             isSet: userData.isAvatarImageSet,
             image: userData.avatarImage,
-          });
+        });
+    } catch (err) {
+        console.error("========== SET AVATAR ERROR ==========");
+        console.error(err);
 
-
-    }catch(err){
-        next(err);
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        });
     }
-}
+};
 
 export const allUsers = async (req, res, next) => {
-    try{
-        const user = await User.find({_id: {$ne: req.params.id}}).select([
+    try {
+        const users = await User.find({
+            _id: { $ne: req.params.id },
+        }).select([
             "email",
             "username",
             "avatarImage",
             "_id",
         ]);
 
-        return res.json(user);
+        return res.json(users);
+    } catch (err) {
+        console.error("========== ALL USERS ERROR ==========");
+        console.error(err);
+
+        return res.status(500).json({
+            success: false,
+            message: err.message,
+        });
     }
-    catch(err){
-        next(err);
-    }
-}
+};
